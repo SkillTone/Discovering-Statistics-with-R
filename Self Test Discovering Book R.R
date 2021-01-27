4d9fe0d876518083bbce9c4e85aa56aaecd57bba
SkillTone <75940692+SkillTone@users.noreply.github.com>
2021-01-27 06:37:01 +0600

    Add files via upload

---
+++ Self Test Discovering Book R.R
@@ -0,0 +1,14 @@
+sust<-("MNA"MUA""ZUA)
+sust<-c("MNA","MUA","ZUA")
+sust
+sust<-sust[sust,!="MNA"]
+sust<-sust[sust !="MNA"]
+print(sust)
+sust<-c(sust,"AMJ")
+print(sust)
+friend<-c("LRB","JAMES","Artcel","Beatls","Metallica")
+print(friend)
+friend<-friend[friend !="LRB"]
+friend
+friend<-c(friend,"MNA")
+friend
